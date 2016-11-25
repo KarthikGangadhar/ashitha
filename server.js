5d9bce38baaf9d2b4c96f7d104c96ae182dda8f5
karthik <karthikg1643@gmail.com>
2016-11-25 15:56:08 +0530

    initiated ashitha app

---
+++ server.js
@@ -0,0 +1,10 @@
+// var path = require('path');
+ console.log(__filename);
+// console.log(`${harsha}`);
+var sum = 0;
+var c = [1,1,2,3,4,5,6,78,9];
+console.log(process.argv);
+c.forEach(function(intgr){
+    sum += intgr;
+     console.log(sum); 
+});
